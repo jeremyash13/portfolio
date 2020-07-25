9aacf91721061c8d268ba2cd27dcaf8b6d03ed9a
Jeremy <jeremyash13@gmail.com>
2020-07-25 16:58:03 -0600

    modified readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Home | My Portfolio'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'My Web Development Portfolio'} />
       </Helmet>
       <App />
     </>
